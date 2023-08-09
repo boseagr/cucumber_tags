31258a2b6f807d4518ab48e6d64ed6baf84a243f
boseagr <52644618+boseagr@users.noreply.github.com>
2023-08-09 16:10:57 +0700

    Delete cucumber_tags directory

--- cucumber_tags/lib/cucumber_tags.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'gherkin/parser'
-require 'byebug'
-require_relative 'cucumber_tags/cucumber_tags'
--- cucumber_tags/lib/cucumber_tags/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module CucumberTags
-  module VERSION
-    STRING = '3.0.1'
-  end
-end
